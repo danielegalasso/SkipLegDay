067c199f4769238f53cc683c6b3c50bab732066a
visci <domenico.visciglia362@gmail.com>
2023-05-07 10:01:48 +0200

    Visci11

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,8 +2,6 @@ module com.example.prog {
     requires javafx.controls;
     requires javafx.fxml;
 
-
-    opens com.example.prog to javafx.fxml;
     exports com.example.skiplegday;
     opens com.example.skiplegday to javafx.fxml;
 }

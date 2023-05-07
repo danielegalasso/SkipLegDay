978ffce415f4ed98f374fb1ddd76ee3b327014d0
visci <domenico.visciglia362@gmail.com>
2023-05-07 12:14:40 +0200

    tutto

--- src/main/java/com/example/skiplegday/HelloApplication.java
+++ src/main/java/com/example/skiplegday/HelloApplication.java
@@ -1,5 +1,6 @@
 package com.example.skiplegday;
 
+import com.example.skiplegday.view.SceneHandler;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
@@ -10,11 +11,8 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("prog.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 800, 600);
-        stage.setTitle("Hello Federico magari muori veramente! Federi Federi succhiami sto cazzo. FEDERICO PC. 222tbùòk 5tgùòk yhofevgbèouref");
-        stage.setScene(scene);
-        stage.show();
+        SceneHandler.getInstance().init(stage);
+        SceneHandler.getInstance().createLoginScene();
     }
 
     public static void main(String[] args) {
---
+++ src/main/java/com/example/skiplegday/view/SceneHandler.java
@@ -0,0 +1,42 @@
+package com.example.skiplegday.view;
+
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class SceneHandler {
+    private static final SceneHandler instance = new SceneHandler();
+    private SceneHandler() {}
+    public static SceneHandler getInstance() {return instance;}
+    private Stage stage;
+    private Scene scene;
+    public void init(Stage stage) {
+        if(this.stage==null){
+            this.stage = stage;
+            this.stage.setTitle("SkipLegDay");
+            createLoginScene();  //funzione che imposta la scene dello SceneHandler
+            this.stage.setScene(scene);
+            this.stage.show();
+        }
+    }
+    private<T> T loadRootFromFXML(String resourceName) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(SceneHandler.class.getResource("/com/example/skiplegday/"+resourceName));
+        return fxmlLoader.load();
+    }
+    public void createLoginScene() {
+        try {
+            if(scene == null) {
+                scene = new Scene(loadRootFromFXML("login.fxml"));
+            }
+            else   //se non c'è la creo altrimenti la modifico
+                scene.setRoot(loadRootFromFXML("login.fxml"));
+            stage.setWidth(400);
+            stage.setHeight(350);
+            stage.setResizable(false);
+        } catch (IOException ignored) {
+        }
+    }
+
+}

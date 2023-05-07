967ebb777850549af9b41c99f339be29071f9e82
fefe2 <dfrfrc02e23c349m@studenti.unical.it>
2023-05-07 09:36:41 +0200

    federico commit

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,8 +1,10 @@
-module com.example.skiplegday {
+module com.example.prog {
     requires javafx.controls;
     requires javafx.fxml;
 
 
-    opens com.example.skiplegday to javafx.fxml;
+    opens com.example.prog to javafx.fxml;
+    exports com.example.prog;
     exports com.example.skiplegday;
+    opens com.example.skiplegday to javafx.fxml;
 }

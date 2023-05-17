38e19f4104b338f2c3a7bfbe8ede0fc8c7d2b1c8
visci <domenico.visciglia362@gmail.com>
2023-05-17 23:28:08 +0200

    model + altro

--- src/main/java/com/example/skiplegday/view/SceneSecondaryHandler.java
+++ src/main/java/com/example/skiplegday/view/SceneSecondaryHandler.java
@@ -145,7 +145,7 @@ public class SceneSecondaryHandler {
         columnIndex = 0;
         for (Node node : children) {
             // Riposiziona il nodo nel GridPane
-            addSchedaNextPositionGridPane(node); //chidi
+            addSchedaNextPositionGridPane(node);
         }
     }
     public void setGridPaneSchede(GridPane gridPane) {

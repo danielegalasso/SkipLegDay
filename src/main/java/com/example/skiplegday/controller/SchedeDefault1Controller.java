52f6f703ae7ab948571ed571b8e8fcdd377b23dc
Daniele Galasso <daniele.galasso03@gmail.com>
2023-06-06 10:49:33 +0200

    ricerca testo in statistica grossa

---
+++ src/main/java/com/example/skiplegday/controller/SchedeDefault1Controller.java
@@ -0,0 +1,13 @@
+package com.example.skiplegday.controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.Node;
+import javafx.scene.layout.GridPane;
+
+public class SchedeDefault1Controller {
+    @FXML
+    GridPane gridPaneDefault;
+    public void addSchedaDefault(Node node,int i,int j) {
+        gridPaneDefault.add(node,i,j);
+    }
+}
